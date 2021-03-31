41e3cc1b939d0e23e8f1b15b6f567a66920305c7
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2021-03-31 18:30:51 +0200

    added player variable

--- src/main/java/de/hglabor/plugins/ffa/listener/FFAQuitListener.java
+++ src/main/java/de/hglabor/plugins/ffa/listener/FFAQuitListener.java
@@ -2,6 +2,7 @@ package de.hglabor.plugins.ffa.listener;
 
 import de.hglabor.plugins.ffa.player.FFAPlayer;
 import de.hglabor.plugins.ffa.player.PlayerList;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -11,12 +12,13 @@ public class FFAQuitListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         event.setQuitMessage(null);
-        FFAPlayer ffaPlayer = PlayerList.getInstance().getPlayer(event.getPlayer());
+        Player player = event.getPlayer();
+        FFAPlayer ffaPlayer = PlayerList.getInstance().getPlayer(player);
         ffaPlayer.getKits().forEach(kit -> kit.onDisable(ffaPlayer));
         if (ffaPlayer.isInCombat()) {
-            event.getPlayer().setHealth(0);
+            player.setHealth(0);
         }
-        PlayerList.getInstance().remove(event.getPlayer());
+        PlayerList.getInstance().remove(player);
     }
 }
 
